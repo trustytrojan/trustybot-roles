6d4fd481527dac52ce91131a165a149dab2005b6
trustytrojan <tarbot24@hotmail.com>
2022-12-20 16:11:29 -0500

    push

--- command-data.js
+++ command-data.js
@@ -9,19 +9,17 @@ const sr_option = {
   required: true
 };
 
-function add_roles_to_options(role_arr, start, end) {
-  for(let i = start; i <= end; ++i) {
-    role_arr.push({ type: Role, name: `role_${i}`, description: `role ${i}` });
-  }
+const role_1 = { type: Role, name: 'role_1', description: 'role 1 (required)', required: true };
+
+const roles_10 = [role_1];
+for(let i = 2; i <= 10; ++i) {
+  roles_10.push({ type: Role, name: `role_${i}`, description: `role ${i}` });
 }
 
-const roles_10 = [
-  { type: Role, name: 'role_1', description: 'role 1 (required)', required: true }
-];
-add_roles_to_options(roles_10, 2, 10);
-
-const roles_25 = [...roles_10];
-add_roles_to_options(roles_25, 11, 25);
+const roles_24 = [...roles_10];
+for(let i = 11; i <= 24; ++i) {
+  roles_24.push({ type: Role, name: `role_${i}`, description: `role ${i}` });
+}
 
 export const global_commands = [
   { name: 'ping', description: 'check ping' }
@@ -33,6 +31,7 @@ export const guild_commands = [
   { name: 'mass_manage_roles', description: '(manage roles required) add/remove roles to/from all members', options: [
     { name: 'action', type: String, description: 'do you want to add or remove roles from all members?', choices: [
       { name: 'add', value: 'add' }, { name: 'remove', value: 'remove' }
-    ] }
+    ] },
+    ...roles_24
   ] },
 ];
