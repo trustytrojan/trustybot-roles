0332dd64a7329d3f70d16289f3225db3293489bf
trustytrojan <tarbot24@hotmail.com>
2022-12-20 22:32:41 -0500

    push

--- any-button-interaction.js
+++ any-button-interaction.js
@@ -1,22 +1,29 @@
-import { ButtonInteraction } from 'discord.js';
+import {
+  ButtonInteraction,
+  GuildMember
+} from 'discord.js';
+
 import './reply-ephemeral.js';
 
 export default async function any_button_interaction(
   /** @type {ButtonInteraction} */ interaction
 ) {
-  const { message, customId, guild, member } = interaction;
-
+  const { message, customId, guild } = interaction;
   if(!guild) return;
 
-  if((await guild.roles.fetch())?.has(customId)) {
-    if(member.roles.cache.has(customId)) {
+  let { member } = interaction;
+  if(!(member instanceof GuildMember))
+    member = await guild.members.fetch(member.user.id);
+
+  if(guild.roles.resolve(customId)) {
+    if(member.roles.resolve(customId)) {
       await member.roles.remove(customId);
       interaction.replyEphemeral(`removed <@&${customId}>!`);
     } else {
       let replaced;
-      if(message.content.includes('||single_rol||'))
+      if(message.content.includes('||single_role||'))
         for(const { customId: role } of message.components[0].components) {
-          if(member.roles.cache.has(role)) {
+          if(member.roles.resolve(role)) {
             await member.roles.remove(role);
             replaced = `replaced <@&${role}> with <@&${customId}>!`;
           }
