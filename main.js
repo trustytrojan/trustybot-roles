df547b95fdafb9e77f1c2b60e36ca1b51d7c700b
trustytrojan <tarbot24@hotmail.com>
2022-12-20 20:24:46 -0500

    push

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import trustybot from './trustybot.js';
 import SingleRole from './SingleRole.js';
-import button_roles from './commands/button-roles.js';
 import token from './token.json' assert { type: 'json' };
+
+import view_roles from './commands/view-roles.js';
+import button_roles from './commands/button-roles.js';
 import any_button_interaction from './any-button-interaction.js';
 import mass_manage_roles from './commands/mass-manage-roles.js';
 
@@ -17,6 +19,7 @@ const client = new trustybot({
 const { chat_input, button } = client;
 
 chat_input.on('ping', (i) => i.reply(`\`${client.ws.ping}ms\``));
+chat_input.on('view_roles', (i) => view_roles(i));
 chat_input.on('button_roles', (i) => button_roles(i, single_roles));
 chat_input.on('mass_manage_roles', (i) => mass_manage_roles(i));
 
